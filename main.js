46d775efbe102c3778213a23d1a30850c26b9bbb
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2023-01-10 16:05:29 +0400

    ----------------------------END OF ABOUT-->

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 //получаю список событий
 window.addEventListener('scroll',()=> {
-document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY>0) //вступит в силу когда окна увеличиваются по оси y  больше чем на 0
+document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0); //вступит в силу когда окна увеличиваются по оси y  больше чем на 0
+})
+
+
+//contact buttons
+const  textButtons =document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector ('p');
+
+text.innerHTML.split('').map((character, index)=>`<span style="transform: rotate(${index * 12}deg)'>${character}</span>`).join()
 })
